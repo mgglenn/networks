1c1969afb016c4319369fa52b3bc7531be533b76
Grace Glenn <mgglenn@g.clemson.edu>
2015-10-31 17:10:50 -0400

    finish client

--- hw2/getfile.c
+++ hw2/getfile.c
@@ -62,14 +62,30 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
+	/* CREATE STREAM SOCKET USING TCP */
 	if((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP) < 0)) {
 		DieWithError("socket() failed\n");
 	}
 
+	/* CONSTRUCT SERVER ADDRESS STRUCTURE */
 	memset(&webServAddr, 0, sizeof(webServAddr));
 	webServAddr.sin_family = AF_INET;
 	webServAddr.sin_addr.s_addr = inet_addr(servIP);
 	webServAddr.sin_port = htons(webServPort);
+	
+	/* RESOLVE DOTTED DECIMAL ADDRESS */
+	if (webServAddr.sin_addr.s_addr == -1) {
+		host = gethostbyname(servIP);
+		webServAddr.sin_addr.s_addr = *((unsigned long *) host->h_addr_list[0]);
+	}
+
+	/* ESTABLISH CONNECTION */
+	if(connect(sock, (struct sockaddr *) &webServAddr, sizeof(webServAddr)) < 0 )
+		DieWithError("connect() failed\n");
+
+	/* SEND TO SERVER */
+
+	/* RECEIVE SAME STRING */
 
 	close (sock);
 	exit(0);
