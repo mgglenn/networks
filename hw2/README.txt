5d666ceed3bc73b75ee7f17c349037fc1eb571c6
Grace Glenn <mgglenn@g.clemson.edu>
2015-11-01 18:16:21 -0500

    parsing url works

---
+++ hw2/README.txt
@@ -0,0 +1,9 @@
+Mary Grace Glenn (mgglenn)
+CPSC 3600 Homework 2
+Sekou Remy
+Fall 2015
+
+*******************************************************************************
+		SIMPLE TCP HTTP CLIENT AND WEBSERVER PROGRAM
+*******************************************************************************
+
--- hw2/getfile.c
+++ hw2/getfile.c
@@ -1,10 +1,10 @@
-/**
+/*
 * Mary Grace Glenn (mgglenn)
 * CPSC 3600 Homework 2
 * 
 * getfile.c
 *
-* Provides a simple TCP client
+* Provides a simple TCP client that sends HTTP get requests.
 */
 
 #include <stdio.h>
@@ -16,36 +16,24 @@
 #include <string.h>
 #include <unistd.h>
 
-#define RCVBUFSIZE 32 /* max length is 31 + null char */
-
 void DieWithError(char *errMessage);
 
 int main(int argc, char *argv[]) {
 
 	int sock;
-	struct sockaddr_in webServAddr;
 	unsigned short webServPort = 8080;
-	struct hostent *host;
-	char *servIP;
-	char *message;
-	char *messageBuffer[RCVBUFSIZE];
-	unsigned int messageStringLen;
-	int bytesRcvd;
-	int totalBytesRcvd;
 	char *filename;
 
-	/* CHECK PARAMETERS */
+	/* CHECK PARAMETERS 
 
 	if (( argc < 2) || ( argc > 6 )) {	
 		fprintf(stderr, "Usage: %s < URL > [-p port] [-f filename]\n", argv[0]);
 		fprintf(stderr, "URL (required): requested URL\n");
 		fprintf(stderr, "port (optional): web server port, default 8080\n");
-		fprintf(stderr, "filename (optional): doc saved as textual file\n");
+		fprintf(stderr, "filename (optional): doc saved as text file\n");
 		exit(1);
 	}
 	else {
-		servIP = argv[1];
-
 		if(argc >= 4) {
 			int i = 4;
 			while (i <= argc) {
@@ -61,32 +49,68 @@ int main(int argc, char *argv[]) {
 			}
 		}
 	}
+*/
+	char *temp;
+	char *hostName;
+	char *pageName;
 
-	/* CREATE STREAM SOCKET USING TCP */
+	temp = strstr(argv[1], "www.");
+	strcpy(pageName, strchr(temp, '/') + 1);
+
+	temp = strstr(argv[1], "www.");
+	hostName = strtok(temp, "/");
+
+	printf("TEMP NAME: %s\n", temp);
+	printf("HOST NAME: %s\n", hostName);	
+	printf("PAGE NAME: %s\n", pageName);
+	
+/* SOCKET SHIT */
+
 	if((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP) < 0)) {
 		DieWithError("socket() failed\n");
 	}
+	else {
+		printf("socket made...\n");
+	}
 
-	/* CONSTRUCT SERVER ADDRESS STRUCTURE */
+
+/* RESOLVE IP ADDRESS 
+
+	char *servIP = (char *)malloc(16);
+	memset(servIP, 0, 16);
+	struct hostent *host;
+
+	if((host = gethostbyname(hostName)) == NULL) {
+		DieWithError("couldn't resolve IP address");
+	}
+
+	if(inet_ntop(AF_INET, (void *)host->h_addr_list[0], servIP, 15) == NULL){
+		DieWithError("couldn't resolve host");
+	}
+*/
+
+//	printf("IP: %s\n", servIP);
+
+/* MAKE SERVER ADDRESS */
+
+	//struct sockaddr_in webServAddr;	
+/*
 	memset(&webServAddr, 0, sizeof(webServAddr));
 	webServAddr.sin_family = AF_INET;
 	webServAddr.sin_addr.s_addr = inet_addr(servIP);
 	webServAddr.sin_port = htons(webServPort);
-	
-	/* RESOLVE DOTTED DECIMAL ADDRESS */
-	if (webServAddr.sin_addr.s_addr == -1) {
+*/
+/*
+RESOLVE SERVIP 
+
+	if (webServAddr.sin_addr.s_addr < 0) {
 		host = gethostbyname(servIP);
 		webServAddr.sin_addr.s_addr = *((unsigned long *) host->h_addr_list[0]);
 	}
 
-	/* ESTABLISH CONNECTION */
 	if(connect(sock, (struct sockaddr *) &webServAddr, sizeof(webServAddr)) < 0 )
 		DieWithError("connect() failed\n");
-
-	/* SEND TO SERVER */
-
-	/* RECEIVE SAME STRING */
-
+*/	
 	close (sock);
 	exit(0);
 }
