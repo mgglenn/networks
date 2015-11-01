d9217531be9a039f5616896b7640b5a12cac02aa
Grace Glenn <mgglenn@g.clemson.edu>
2015-11-01 15:06:49 -0500

    sever param parasing

---
+++ hw2/webserver.c
@@ -0,0 +1,84 @@
+/*
+* Mary Grace Glenn (mgglenn)
+* CPSC 3600 homework 2
+*/
+
+#include <stdio.h>      /* for printf() and fprintf() */
+#include <sys/socket.h> /* for socket(), bind(), and connect() */
+#include <arpa/inet.h>  /* for sockaddr_in and inet_ntoa() */
+#include <stdlib.h>     /* for atoi() and exit() */
+#include <string.h>     /* for memset() */
+#include <unistd.h>     /* for close() */
+
+void DieWithError(char *errorMessage);
+
+int main(int argc, char *argv[]) {
+
+	int servSock;
+	int clntSock;
+	struct sockaddr_in servAddr;
+	struct sockaddr_in clntAddr;
+	unsigned short servPort;
+	char *root;
+	unsigned int clntLen;
+
+	/* ./arg1.out -p 12342 server */ 
+
+	if(argc < 4) {
+		fprintf(stderr, "Usage: %s < Server Port >\n", argv[0]);
+		exit(1);
+	}
+	else {
+		int i = 1;
+		while(i < argc -1) {
+			if(strcmp("-p", argv[i]) == 0) {
+				servPort = atoi(argv[i + 1]);
+				fprintf(stderr, "port resolved\n");
+			}
+			else {
+				root = argv[i];	
+				fprintf(stderr, "root resolved\n");	
+			}
+			i++;
+		}
+	}
+
+	servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	
+	if(servSock < 0) {
+		DieWithError("socket() failed");
+	}
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servAddr.sin_port = htons(servPort);
+
+	/* Bind to local address */
+
+	if(bind(servSock, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0) {
+		DieWithError("bind() failed");
+	}
+
+	if(listen(servSock, 5) < 0) {
+		DieWithError("listen failed");
+	}
+
+	while(1) {
+		clntLen = sizeof(clntAddr);
+
+		clntSock = accept(servSock, (struct sockaddr *) &clntAddr, &clntLen);
+
+		if(clntSock < 0) {
+			DieWithError("accept failed");
+		}
+		else {
+			printf("client connected! \n");
+		}
+	}
+}
+
+void DieWithError(char *errorMessage) {
+	fprintf(stderr, "%s\n", errorMessage);
+	exit(0);
+}
